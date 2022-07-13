92d68211376799722de870c1d72e7f34086bd876
Adrian Fadiga <adrianmfadiga@gmail.com>
2022-07-12 23:39:32 -0300

    refactor: alterado nome das constantes

--- app/backend/src/database/utils/utils.ts
+++ app/backend/src/database/utils/utils.ts
@@ -3,6 +3,13 @@ import ITeam from '../interfaces/ITeam';
 import Leaderboard from '../types/Leaderboard';
 import homeOrAwayType from '../types/HomeOrAway';
 
+const sortLeaderboards = (leaderboards: Leaderboard[]) => leaderboards
+  .sort((a, b) => b.totalPoints - a.totalPoints
+|| b.totalVictories - a.totalVictories
+|| b.goalsBalance - a.goalsBalance
+|| b.goalsFavor - a.goalsFavor
+|| b.goalsOwn - a.goalsOwn);
+
 const teamObj: Leaderboard = {
   name: '',
   id: 0,
@@ -16,63 +23,48 @@ const teamObj: Leaderboard = {
   goalsBalance: 0,
   efficiency: 0,
 };
+const generateTeamsArray = (matches: IMatch[], teams: ITeam[], homeOrAway: homeOrAwayType) =>
+  matches.reduce((acc: Leaderboard[], curr) => {
+    const currentTeam = teams.find((team) => team.id === curr[homeOrAway]);
+    if (acc.every((team) => team.id !== curr[homeOrAway])) {
+      teamObj.name = currentTeam?.teamName;
+      teamObj.id = curr[homeOrAway];
+      acc.push({ ...teamObj });
+    }
+    return acc;
+  }, []);
 
-const sortLeaderboards = (leaderboards: Leaderboard[]) => leaderboards
-  .sort((a, b) => b.totalPoints - a.totalPoints
-    || b.totalVictories - a.totalVictories
-    || b.goalsBalance - a.goalsBalance
-    || b.goalsFavor - a.goalsFavor
-    || b.goalsOwn - a.goalsOwn);
-
-const generateTeamsArray = (
-  matches: IMatch[],
-  teams: ITeam[],
-  homeOrAway: homeOrAwayType,
-) => matches.reduce((acc: Leaderboard[], curr) => {
-  const xablau = teams.find((t) => t.id === curr[homeOrAway]);
-  if (acc.every((team) => team.id !== curr[homeOrAway])) {
-    teamObj.name = xablau?.teamName;
-    teamObj.id = curr[homeOrAway];
-    acc.push({ ...teamObj });
-  }
-  return acc;
-}, []);
-
-const updateHomeTeamScore = (teamsArray: Leaderboard[], matches: IMatch[]) => {
-  const teste = matches.reduce((acc, curr) => {
-    const xablau = acc.find((team) => team.id === curr.homeTeam);
-    if (!xablau) return acc;
-    if (curr.homeTeamGoals > curr.awayTeamGoals) xablau.totalVictories += 1;
-    if (curr.homeTeamGoals === curr.awayTeamGoals) xablau.totalDraws += 1;
-    if (curr.homeTeamGoals < curr.awayTeamGoals) xablau.totalLosses += 1;
-    xablau.totalPoints = (xablau.totalVictories * 3) + xablau.totalDraws;
-    xablau.totalGames += 1;
-    xablau.goalsFavor += curr.homeTeamGoals;
-    xablau.goalsOwn += curr.awayTeamGoals;
-    xablau.goalsBalance = xablau.goalsFavor - xablau.goalsOwn;
-    xablau.efficiency = (xablau.totalPoints / (xablau.totalGames * 3)) * 100;
+const updateHomeTeamScore = (teamsArray: Leaderboard[], matches: IMatch[]) =>
+  matches.reduce((acc, curr) => {
+    const currentTeam = acc.find((team) => team.id === curr.homeTeam);
+    if (!currentTeam) return acc;
+    if (curr.homeTeamGoals > curr.awayTeamGoals) currentTeam.totalVictories += 1;
+    if (curr.homeTeamGoals === curr.awayTeamGoals) currentTeam.totalDraws += 1;
+    if (curr.homeTeamGoals < curr.awayTeamGoals) currentTeam.totalLosses += 1;
+    currentTeam.totalPoints = (currentTeam.totalVictories * 3) + currentTeam.totalDraws;
+    currentTeam.totalGames += 1;
+    currentTeam.goalsFavor += curr.homeTeamGoals;
+    currentTeam.goalsOwn += curr.awayTeamGoals;
+    currentTeam.goalsBalance = currentTeam.goalsFavor - currentTeam.goalsOwn;
+    currentTeam.efficiency = (currentTeam.totalPoints / (currentTeam.totalGames * 3)) * 100;
     return acc;
   }, teamsArray);
-  return teste;
-};
 
-const updateAwayTeamScore = (teamsArray: Leaderboard[], matches: IMatch[]) => {
-  const teste = matches.reduce((acc, curr) => {
-    const xablau = acc.find((team) => team.id === curr.awayTeam);
-    if (!xablau) return acc;
-    if (curr.homeTeamGoals < curr.awayTeamGoals) xablau.totalVictories += 1;
-    if (curr.homeTeamGoals === curr.awayTeamGoals) xablau.totalDraws += 1;
-    if (curr.homeTeamGoals > curr.awayTeamGoals) xablau.totalLosses += 1;
-    xablau.totalPoints = (xablau.totalVictories * 3) + xablau.totalDraws;
-    xablau.totalGames += 1;
-    xablau.goalsFavor += curr.awayTeamGoals;
-    xablau.goalsOwn += curr.homeTeamGoals;
-    xablau.goalsBalance = xablau.goalsFavor - xablau.goalsOwn;
-    xablau.efficiency = (xablau.totalPoints / (xablau.totalGames * 3)) * 100;
+const updateAwayTeamScore = (teamsArray: Leaderboard[], matches: IMatch[]) =>
+  matches.reduce((acc, curr) => {
+    const currentTeam = acc.find((team) => team.id === curr.awayTeam);
+    if (!currentTeam) return acc;
+    if (curr.homeTeamGoals < curr.awayTeamGoals) currentTeam.totalVictories += 1;
+    if (curr.homeTeamGoals === curr.awayTeamGoals) currentTeam.totalDraws += 1;
+    if (curr.homeTeamGoals > curr.awayTeamGoals) currentTeam.totalLosses += 1;
+    currentTeam.totalPoints = (currentTeam.totalVictories * 3) + currentTeam.totalDraws;
+    currentTeam.totalGames += 1;
+    currentTeam.goalsFavor += curr.awayTeamGoals;
+    currentTeam.goalsOwn += curr.homeTeamGoals;
+    currentTeam.goalsBalance = currentTeam.goalsFavor - currentTeam.goalsOwn;
+    currentTeam.efficiency = (currentTeam.totalPoints / (currentTeam.totalGames * 3)) * 100;
     return acc;
   }, teamsArray);
-  return teste;
-};
 
 const updateTeamScore = (
   teamsArray: Leaderboard[],
